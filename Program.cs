0cc0fd4dca8d5faee31313ae762b8d02ae224d30
LanceloT7730 <ac2952602@akfauniversity.org>
2022-08-26 08:44:50 +0500

    finished)

--- Program.cs
+++ Program.cs
@@ -4,13 +4,42 @@ namespace Palindrome_Challenge
 {
     class Program
     {
-        public void isPalindrome(string txt)
-        {
-
-        }
+        
         static void Main(string[] args)
         {
-            
+            Program d = new Program();
+            d.display();
+        }
+
+        public void isPalindrome(string txt)
+        {
+            int size = txt.Length;
+            bool condition = true;
+            for (int i = 0; i < size; i++)
+            {
+                if (txt[i] == txt[(size - 1) - i])
+                {
+                    condition = true;
+                }
+                else
+                {
+                    condition = false;
+                    size = 0;
+                }
+                
+            }
+            Console.WriteLine($"Palindrome: {condition}, Length: {size}");
+        }
+        public void display()
+        {
+            Console.WriteLine("Let's begin");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "exit") break;
+                Program p = new Program();
+                p.isPalindrome(input);
+            }
         }
     }
 }
