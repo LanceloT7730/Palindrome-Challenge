a008c5747dbed84d25621d3022cbccb742dc6bfd
LanceloT7730 <ac2952602@akfauniversity.org>
2022-08-26 08:12:35 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Palindrome_Challenge
+{
+    class Program
+    {
+        public void isPalindrome(string txt)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
